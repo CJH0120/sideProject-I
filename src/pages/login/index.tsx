003217bf632a7438881d8e94dbf31faa071feb09
jong hyun <rhrhdksk@gmail.com>
2023-02-23 00:47:39 +0900

    [2023/02/23] -Add Login Page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,37 +1,57 @@
-import Layout from 'components/layouts/Layouts'
 import Style from '@/styles/Login.module.scss'
 import classNames from 'classnames/bind'
 import { NextPage } from 'next'
-import IconDefault from 'components/icons/IconDefaultImg'
 import IconLogo from 'components/icons/IconLogo'
+import Button from 'components/control/Button'
+import { useEffect } from 'react'
+import Link from 'next/link'
 const cx = classNames.bind(Style)
+
 const Login: NextPage = () => {
+  useEffect(() => {
+    document.body.style.overflow = 'hidden'
+  }, [])
   return (
     <section className={cx('login-wrap')}>
       <div className={cx('login')}>
-        <div className={cx('login-head')}>
+        <Link href={'/'} className={cx('login-head')}>
           <span className={cx('logo')}>
             <IconLogo className={cx('icon-logo')} />
           </span>
           <h1 className={cx('logo-text')}>Petty</h1>
-        </div>
+        </Link>
         <div className={cx('login-contnet')}>
           <div className={cx('login-input-wrap')}>
             <div className={cx('login-input')}>
-              <label>
-                <p>아이디</p>
-                <input type={'text'} placeholder="아이디" />
+              <label className={cx('label-wrap')} htmlFor="inputId">
+                이메일
               </label>
+
+              <input type={'text'} placeholder="이메일" className={cx('input')} id="inputId" />
             </div>
             <div className={cx('login-input', 'p')}>
-              <label>
-                <p>비밀번호</p>
-                <input type={'password'} placeholder="비밀번호" />
+              <label className={cx('label-wrap')} htmlFor="inputpw">
+                비밀번호
               </label>
+              <input type={'password'} placeholder="비밀번호" id="inputpw" className={cx('input')} />
             </div>
           </div>
-          <div>ss</div>
-          <div>ss</div>
+        </div>
+        <div className={cx('find')}>뭐찾고 뭐찾고</div>
+        <div className={cx('button-wrap')}>
+          <Button
+            Classname={cx('btn')}
+            border
+            color="yellow"
+            onClick={() => {
+              location.href = '/'
+            }}
+          >
+            로그인
+          </Button>
+          <Button Classname={cx('btn')} border color="white">
+            회원가입
+          </Button>
         </div>
       </div>
     </section>
